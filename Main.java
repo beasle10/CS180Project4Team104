06c7a7a7cd8a8b0918729f00654133b651fc3973
AnanyaGoel926 <89860952+AnanyaGoel926@users.noreply.github.com>
2022-04-01 15:32:31 -0400

    Update Main.java
    creation of account

--- Main.java
+++ Main.java
@@ -1,27 +1,34 @@
-import Project4.User;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<User> usernames = new ArrayList<>();
+        ArrayList<String> usernames = new ArrayList<>();
         System.out.println("Welcome to the learning management system!");
-        System.out.println("Are you creating, editing, deleting an account? (Enter 1, 2, 3, 4)\n" +
+        do {
+            System.out.println("Are you creating, editing, deleting an account? (Enter 1, 2, 3, 4)\n" +
                     "1. Creating an account\n" +
                     "2. Editing an account\n" +
                     "3. Deleting an account\n" +
-                    "4. Exit\n");
-        String initialSelection = scanner.nextLine();
-        if (initialSelection.equals("1")){
-            System.out.println("Please enter Username");
-            String username = scanner.nextLine();
-            
+                    "4. Exit");
+            String initialSelection = scanner.nextLine();
+            if (initialSelection.equals("1")){
+                System.out.println("Please enter Username");
+                String username = scanner.nextLine();
+                for (int i = 0; i < usernames.size(); i++) {
+                    if (usernames.get(i).equals(username)) {
+                        System.out.println("Username already taken. Please try again!");
+                    }
+                }
+                System.out.println("Please enter Password");
+                String password = scanner.nextLine();
+                usernames.add(Integer.parseInt(username),password);
+            } else if (initialSelection.equals("2")){
 
-        }
+            }
 
-
+        } while(true);
 
 
     }
